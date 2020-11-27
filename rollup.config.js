9476d041aaa867d236c8d6aa2cc6f1aaab7b4165
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-11-27 12:01:46 +0700

    Switch to `rollup-plugin-flat-dts`

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import flatDts from '@proc7ts/rollup-plugin-flat-dts';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import flatDts from 'rollup-plugin-flat-dts';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
