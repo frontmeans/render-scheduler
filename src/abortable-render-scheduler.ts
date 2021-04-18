aab66d88c2c586a6b26f0fd3a224de23b959df0b
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-04-18 18:03:00 +0700

    Implement abortable render scheduler

---
+++ src/abortable-render-scheduler.ts
@@ -0,0 +1,174 @@
+import { Supply } from '@proc7ts/supply';
+import type { RenderSchedule, RenderScheduleOptions } from './render-schedule';
+import { RenderScheduleConfig } from './render-schedule';
+import type { RenderScheduler } from './render-scheduler';
+import type { RenderExecution, RenderShot } from './render-shot';
+
+/**
+ * Options for abortable render schedule.
+ */
+export interface AbortableRenderScheduleOptions extends RenderScheduleOptions {
+
+  /**
+   * Render schedule supply.
+   *
+   * Stops rendering in created schedule once cut off.
+   *
+   * A new supply will be created when omitted.
+   */
+  readonly supply?: Supply;
+
+}
+
+/**
+ * Abortable render shot execution context.
+ *
+ * This is passed to {@link RenderShot render shot} when the latter executed by {@link AbortableRenderScheduler
+ * abortable} render scheduler.
+ */
+export interface AbortableRenderExecution extends RenderExecution {
+
+  /**
+   * Render schedule supply.
+   *
+   * Stops rendering in current schedule once cut off.
+   *
+   * This is the same as {@link AbortableRenderSchedule.supply}.
+   */
+  readonly supply: Supply;
+
+}
+
+/**
+ * A render schedule that can be aborted.
+ *
+ * Created by {@link AbortableRenderScheduler abortable render scheduler}.
+ *
+ * @typeParam TExecution - A type of supported render shot execution context.
+ */
+export interface AbortableRenderSchedule<TExecution extends RenderExecution = RenderExecution>
+    extends RenderSchedule<TExecution & AbortableRenderExecution> {
+
+  /**
+   * A supply of this render schedule.
+   *
+   * Stops rendering in this schedule once cut off.
+   *
+   * This is either a supply passed as an {@link AbortableRenderScheduleOptions.supply option}, or the one created for
+   * this schedule when omitted.
+   */
+  readonly supply: Supply;
+
+}
+
+/**
+ * A render scheduler that allows to abort scheduling and execution.
+ *
+ * Can be created by {@link newAbortableRenderScheduler} function.
+ *
+ * @typeParam TExecution - A type of supported render shot execution context.
+ * @typeParam TOptions - A type of accepted render schedule options.
+ */
+export interface AbortableRenderScheduler<
+    TExecution extends RenderExecution = RenderExecution,
+    TOptions extends RenderScheduleOptions = RenderScheduleOptions> {
+
+  /**
+   * Creates an {@link AbortableRenderSchedule abortable} render schedule according to the given options.
+   *
+   * @param options - Created render schedule options.
+   *
+   * @returns New render schedule.
+   */
+  (this: void, options?: TOptions & AbortableRenderScheduleOptions): AbortableRenderSchedule<TExecution>;
+
+  /**
+   * A supply of this render scheduler.
+   *
+   * Once cut off, stops rendering in all schedules created by this scheduler.
+   *
+   * This is either a supply passed to {@link newAbortableRenderScheduler} function, or the one created for this
+   * scheduler when omitted.
+   */
+  readonly supply: Supply;
+
+}
+
+/**
+ * Creates an abortable render schedule.
+ *
+ * @typeParam TExecution - A type of supported render shot execution context.
+ * @typeParam TOptions - A type of accepted render schedule options.
+ * @param scheduler - A render scheduler to schedule rendering by.
+ * @param supply - Schedule supply. Once cut off, stops rendering in all schedules created by new scheduler.
+ * A new supply will be created if omitted.
+ *
+ * @returns New abortable render scheduler instance.
+ */
+export function newAbortableRenderScheduler<
+    TExecution extends RenderExecution,
+    TOptions extends RenderScheduleOptions,
+    >(
+    scheduler: RenderScheduler<TExecution, TOptions>,
+    supply = new Supply(),
+): AbortableRenderScheduler<TExecution, TOptions> {
+
+  const abortableScheduler = ((
+      options?: TOptions & AbortableRenderScheduleOptions,
+  ): AbortableRenderSchedule<TExecution> => {
+
+    const scheduleSupply = options?.supply || new Supply();
+
+    scheduleSupply.needs(supply);
+
+    const schedule = scheduler(options);
+    let execute = (shot: RenderShot<TExecution & AbortableRenderExecution>, exec: TExecution): void => {
+
+      const abortableExec: TExecution & AbortableRenderExecution = {
+        ...exec,
+        postpone(postponed: RenderShot<TExecution & AbortableRenderExecution>) {
+          exec.postpone(_exec => postponed(abortableExec));
+        },
+        supply: scheduleSupply,
+      };
+
+      shot(abortableExec);
+    };
+    let doSchedule = (
+        shot: RenderShot<TExecution & AbortableRenderExecution>,
+    ): void => schedule(
+        exec => execute(shot, exec),
+    );
+    const abortableSchedule = ((
+        shot: RenderShot<TExecution & AbortableRenderExecution>,
+    ): void => doSchedule(shot)) as AbortableRenderSchedule<TExecution>;
+
+    (abortableSchedule as { supply: Supply }).supply = scheduleSupply;
+    scheduleSupply.whenOff(reason => {
+      execute = AbortableRenderSchedule$doNotExecute;
+      doSchedule = AbortableRenderSchedule$abort(reason, options);
+    });
+
+    return abortableSchedule;
+  }) as AbortableRenderScheduler<TExecution, TOptions>;
+
+  (abortableScheduler as { supply: Supply }).supply = supply;
+
+  return abortableScheduler;
+}
+
+function AbortableRenderSchedule$abort(
+    reason: unknown,
+    options: RenderScheduleOptions | undefined,
+): (_shot: RenderShot<never>) => void {
+
+  const { error } = RenderScheduleConfig.by(options);
+
+  return _shot => {
+    error('Rendering aborted', reason);
+  };
+}
+
+function AbortableRenderSchedule$doNotExecute(_shot: RenderShot<never>, _exec: RenderExecution): void {
+  // Do not execute in aborted schedule
+}
