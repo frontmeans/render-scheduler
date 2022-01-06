579f39666fcf55017ab47810bc37a8d86bcfbbf1
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-01-06 13:13:08 +0700

    Remove redundant Rollup plug-in

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
 import { externalModules } from '@run-z/rollup-helpers';
 import { defineConfig } from 'rollup';
 import flatDts from 'rollup-plugin-flat-dts';
@@ -16,7 +15,6 @@ export default defineConfig({
       tsconfig: 'tsconfig.main.json',
       cacheRoot: 'target/.rts2_cache',
     }),
-    nodeResolve(),
     sourcemaps(),
   ],
   external: externalModules(),
